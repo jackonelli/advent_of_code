824f21b89c3a732f633ca4099558337f8a7cdf2f
Jakob <jakob.lindqvist@chalmers.se>
2019-12-03 17:49:29 +0100

    Made Day 2 functional

--- src/bin/2.rs
+++ src/bin/2.rs
@@ -39,55 +39,67 @@ struct Registers {
     output: u32,
 }
 
-fn preprocess_intcode(mut code: Vec<u32>, noun: u32, verb: u32) -> Vec<u32> {
-    code[1] = noun;
-    code[2] = verb;
-    code
+fn preprocess_intcode(code: &Vec<u32>, noun: u32, verb: u32) -> Vec<u32> {
+    let mut new_code = code.clone();
+    new_code[1] = noun;
+    new_code[2] = verb;
+    new_code
 }
 
-fn parse_intcode(mut code: Vec<u32>) -> Vec<u32> {
-    for index in (0..code.len() - 4).step_by(4) {
-        let operation = Opcode::from_sequence(&code[index..(index + 4)]);
-        match operation {
-            Opcode::Sum(Registers { noun, verb, output }) => {
-                code[output as usize] = code[noun as usize] + code[verb as usize];
-            }
-            Opcode::Multiply(Registers { noun, verb, output }) => {
-                code[output as usize] = code[noun as usize] * code[verb as usize];
-            }
-            _ => return code,
+fn update_code(code: Vec<u32>, op_code: Opcode) -> Vec<u32> {
+    let mut new_code = code.clone();
+    match op_code {
+        Opcode::Sum(Registers { noun, verb, output }) => {
+            new_code[output as usize] = code[noun as usize] + code[verb as usize];
+            new_code
         }
+        Opcode::Multiply(Registers { noun, verb, output }) => {
+            new_code[output as usize] = code[noun as usize] * code[verb as usize];
+            new_code
+        }
+        Opcode::Stop => panic!("Here, we should not be"),
     }
-    code
 }
 
-fn brute_force(intcode: Vec<u32>, output: u32) -> (u32, u32) {
-    for noun in (0..100).rev() {
-        for verb in (0..100).rev() {
-            let new_intcode = intcode.clone();
-            let preprocessed = preprocess_intcode(new_intcode, noun, verb);
-            let code = parse_intcode(preprocessed);
-            if code[0] == output {
-                return (noun, verb);
-            }
+fn parse_intcode(address: usize, code: Vec<u32>) -> Vec<u32> {
+    if address + 4 >= code.len() {
+        return code;
+    }
+    let op_code = Opcode::from_sequence(&code[address..(address + 4)]);
+    match op_code {
+        Opcode::Stop => code,
+        _ => {
+            let code = update_code(code, op_code);
+            parse_intcode(address + 4, code)
         }
     }
-    panic!("Did not find any matches");
+}
+
+fn brute_force(intcode: &Vec<u32>, target: u32, noun: u32, verb: u32) -> (u32, u32) {
+    let preprocessed = preprocess_intcode(&intcode, noun, verb);
+    let output = parse_intcode(0, preprocessed)[0];
+    if output == target {
+        (noun, verb)
+    } else if noun == 99 {
+        brute_force(&intcode, target, 0, verb + 1)
+    } else {
+        brute_force(&intcode, target, noun + 1, verb)
+    }
 }
 
 fn star_1() {
     let intcode: Vec<u32> = INTCODE.iter().cloned().collect();
     let noun = 12;
     let verb = 2;
-    let preprocessed = preprocess_intcode(intcode, noun, verb);
-    let code = parse_intcode(preprocessed);
+    let preprocessed = preprocess_intcode(&intcode, noun, verb);
+    let code = parse_intcode(0, preprocessed);
     println!("First element in parsed code: {}", code[0]);
 }
 
 fn star_2() {
     const OUTPUT: u32 = 19_690_720;
     let intcode: Vec<u32> = INTCODE.iter().cloned().collect();
-    let (noun, verb) = brute_force(intcode, OUTPUT);
+    let (noun, verb) = brute_force(&intcode, OUTPUT, 0, 74);
     println!("Input 1: {}, Input 2: {}", noun, verb);
 }
 
@@ -105,7 +117,7 @@ mod tests_2 {
         let noun = 12;
         let verb = 2;
         let true_result_code = vec![1, 12, 2, 0, 99];
-        let preprocessed_code = preprocess_intcode(intcode, noun, verb);
+        let preprocessed_code = preprocess_intcode(&intcode, noun, verb);
         assert_eq!(true_result_code, preprocessed_code);
     }
 
@@ -113,22 +125,22 @@ mod tests_2 {
     fn test_code() {
         let intcode = vec![1, 0, 0, 0, 99];
         let true_result_code = vec![2, 0, 0, 0, 99];
-        let result_code = parse_intcode(intcode);
+        let result_code = parse_intcode(0, intcode);
         assert_eq!(true_result_code, result_code);
 
         let intcode = vec![2, 3, 0, 3, 99];
         let true_result_code = vec![2, 3, 0, 6, 99];
-        let result_code = parse_intcode(intcode);
+        let result_code = parse_intcode(0, intcode);
         assert_eq!(true_result_code, result_code);
 
         let intcode = vec![2, 4, 4, 5, 99, 0];
         let true_result_code = vec![2, 4, 4, 5, 99, 9801];
-        let result_code = parse_intcode(intcode);
+        let result_code = parse_intcode(0, intcode);
         assert_eq!(true_result_code, result_code);
 
         let intcode = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
         let true_result_code = vec![30, 1, 1, 4, 2, 5, 6, 0, 99];
-        let result_code = parse_intcode(intcode);
+        let result_code = parse_intcode(0, intcode);
         assert_eq!(true_result_code, result_code);
     }
 
@@ -136,7 +148,7 @@ mod tests_2 {
     fn test_long_code() {
         let intcode = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
         let true_result_code = vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
-        let result_code = parse_intcode(intcode);
+        let result_code = parse_intcode(0, intcode);
         assert_eq!(true_result_code, result_code);
     }
 }
