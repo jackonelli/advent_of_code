0920424af1170275145e8deb0bba990c1aa7cfc7
Jakob <jakob.lindqvist@chalmers.se>
2020-12-06 08:41:26 +0100

    I understand parsing now

--- 2020/src/bin/6.rs
+++ 2020/src/bin/6.rs
@@ -9,45 +9,37 @@ fn main() {
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("Read to string error");
-    let data = contents.trim().lines();
-    let (acc, last_rec): (Vec<String>, String) =
-        data.fold((Vec::new(), String::new()), |(mut list, mut rec), l| {
-            if l.is_empty() {
-                list.push(rec);
-                (list, String::new())
-            } else {
-                rec.push_str(l);
-                rec.push(' ');
-                (list, rec)
-            }
-        });
-    let acc = [&acc[..], &[String::from(last_rec.trim())]].concat();
-    let count = count_groups(&acc);
-    println!("{}", count);
+
+    let data = contents.trim().split("\n\n");
+    let groups: Vec<Vec<HashSet<char>>> = data
+        .map(|group| {
+            group
+                .split('\n')
+                .map(|person| person.chars().collect::<HashSet<char>>())
+                .collect::<Vec<HashSet<char>>>()
+        })
+        .collect();
+    println!("Star 1: {}", star_1(&groups));
+    println!("Star 2: {}", star_2(&groups));
 }
 
-fn count_groups(groups: &Vec<String>) -> usize {
-    groups.iter().map(|g| count_group(g)).sum()
+fn star_1(group: &[Vec<HashSet<char>>]) -> usize {
+    group.iter().map(|g| unique_ans_in_group(g)).sum()
 }
 
-fn count_group(group: &str) -> usize {
-    let mut sets = group
-        .split(' ')
-        .map(|p| count_person(p))
-        .filter(|p| !p.is_empty());
-    let first = sets.next().unwrap();
-    let count = sets.fold(first, |acc, pers| {
-        acc.intersection(&pers)
-            .map(|x| *x)
-            .collect::<HashSet<char>>()
-    });
-    count.len()
+fn star_2(group: &[Vec<HashSet<char>>]) -> usize {
+    group.iter().map(|g| common_ans_in_group(g)).sum()
 }
 
-fn count_person(person: &str) -> HashSet<char> {
-    person.chars().collect()
+fn unique_ans_in_group(group: &[HashSet<char>]) -> usize {
+    group.iter().flatten().unique().count()
 }
 
-fn star_2(acc: impl Iterator<Item = String>) -> usize {
-    1
+fn common_ans_in_group(group: &[HashSet<char>]) -> usize {
+    let mut sets = group.iter();
+    let first = sets.next().unwrap().clone();
+    sets.fold(first, |acc, pers| {
+        acc.intersection(&pers).cloned().collect::<HashSet<char>>()
+    })
+    .len()
 }
