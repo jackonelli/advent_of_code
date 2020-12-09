a1476b8d4606d9923d8f2412725d4ae5b9e4dd1d
Jakob <jakob.lindqvist@chalmers.se>
2020-12-09 10:36:10 +0100

    Found the Cure for day 9

--- 2020/src/bin/9.rs
+++ 2020/src/bin/9.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 
 fn main() {
     let (window, file) = (25, "input/9/input");
-    //let (window, file) = (5, "input/9/test");
+    // let (window, file) = (5, "input/9/test");
     let mut file = File::open(file).expect("Opening file error");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
@@ -17,7 +17,7 @@ fn main() {
 
     let inv = first_invalid(&data, window);
     println!("Star 1: {}", inv);
-    println!("Star 2: {}", brute(&data, inv));
+    println!("Star 2: {}", star_2(&data, inv));
 }
 
 fn first_invalid(numbers: &[usize], window: usize) -> usize {
@@ -34,28 +34,54 @@ fn sum_exists(numbers: &[usize], sum: usize) -> bool {
         .any(|pair| pair[0] + pair[1] == sum)
 }
 
-fn brute(numbers: &[usize], sum: usize) -> usize {
+fn star_2(numbers: &[usize], target: usize) -> usize {
+    let seq = caterpillar(numbers, target);
+    let largest = seq.iter().max().expect("No max");
+    let smallest = seq.iter().min().expect("No max");
+    smallest + largest
+}
+
+fn caterpillar(numbers: &[usize], target: usize) -> &[usize] {
+    let mut tail = 0;
+    let mut head = 2;
+    let mut sum: usize = numbers[tail..head].iter().sum();
+    loop {
+        assert!(head < numbers.len());
+        while tail < head && sum + numbers[head] > target {
+            sum -= numbers[tail];
+            tail += 1;
+        }
+        while sum + numbers[head] <= target {
+            sum += numbers[head];
+            head += 1;
+        }
+        if sum == target {
+            break;
+        }
+    }
+
+    &numbers[tail..head]
+}
+
+fn _brute(numbers: &[usize], sum: usize) -> usize {
     // Starting from 3 since, in effect, consecutive sum of two is included in the first star.
     let (small, large) = (3..(numbers.len() + 1))
-        .map(|window| find_consec_sum(numbers, window, sum))
+        .map(|window| _find_consec_sum(numbers, window, sum))
         .find(|res| res.is_some())
         .expect("Unwrap to see if 'find' found something")
         .expect("Found no consec sum.");
     small + large
 }
 
-fn find_consec_sum(numbers: &[usize], window: usize, sum: usize) -> Option<(usize, usize)> {
+fn _find_consec_sum(numbers: &[usize], window: usize, sum: usize) -> Option<(usize, usize)> {
     match (0..(numbers.len() - window - 1))
         .map(|i| (i, numbers[i..window + i].iter().sum::<usize>()))
         .find(|(_, consec_sum)| *consec_sum == sum)
     {
         Some((i, _)) => {
-            let mut vec = numbers[i..window + i].to_vec();
-            vec.sort_unstable();
-            Some((
-                *vec.first().expect("No first"),
-                *vec.last().expect("No last"),
-            ))
+            let largest = numbers[i..window + i].iter().max().expect("No max");
+            let smallest = numbers[i..window + i].iter().min().expect("No max");
+            Some((*smallest, *largest))
         }
         None => None,
     }
