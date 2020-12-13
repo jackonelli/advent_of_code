d52663d842a5b345ace4a0892d32d8c2806e1a48
Jakob <jakob.lindqvist@chalmers.se>
2020-12-13 17:43:26 +0100

    Day 13 found extremely stupid bug.

--- 2020/src/bin/13.rs
+++ 2020/src/bin/13.rs
@@ -13,88 +13,14 @@ fn main() {
         .expect("Read to string error");
     let mut data = contents.trim().lines();
     let ts = data.next().unwrap().parse::<usize>().unwrap();
-    let f = 59;
     let raw = data.next().unwrap().split(',').map(|dig| dig.parse());
     let freqs: Vec<usize> = raw
         .clone()
         .filter(Result::is_ok)
         .map(|x| x.unwrap())
         .collect();
-    //println!("Star 1: {}", star_1(ts, &freqs));
+    println!("Star 1: {}", star_1(ts, &freqs));
     println!("Star 2: {}", star_2(raw));
-}
-
-fn check_rems(ts: usize, rem: usize, freq: usize) -> bool {
-    (ts + rem) % freq == 0
-}
-
-fn star_2(raw: impl Iterator<Item = Result<usize, std::num::ParseIntError>>) -> usize {
-    let mut rem_freqs: Vec<(usize, usize)> = raw
-        .enumerate()
-        .filter(|(_idx, f)| f.is_ok())
-        .map(|(idx, f)| (idx, f.unwrap()))
-        .collect();
-    for (rem, f) in &rem_freqs {
-        println!("{}, {:?}", f, rem);
-    }
-    //let (start, period_time) = find_period_time(1, 1, 61, 17);
-
-    rem_freqs.sort_unstable_by_key(|(_rem, f)| std::cmp::Reverse(*f));
-    //rem_freqs.sort_unstable_by_key(|(rem, f)| *f);
-
-    let (rem, freq) = rem_freqs[0];
-    let (mut start, mut period_time) = find_period_time(1, 1, rem, freq);
-    println!("start: {}, period_time: {}", start, period_time);
-    for (rem, freq) in &rem_freqs[1..3] {
-        let res = find_period_time(start, period_time, *rem, *freq);
-
-        start = res.0;
-        period_time = res.1;
-        println!("start: {}, period_time: {}", start, period_time);
-        println!("*****");
-    }
-    star_2_semi(start, period_time, &rem_freqs[3..])
-}
-
-fn star_2_semi(start: usize, period_time: usize, rem_freqs: &[(usize, usize)]) -> usize {
-    let mut i = start;
-    while i < 749468541208439 {
-        if check_all_rems(i, rem_freqs) {
-            return i;
-        };
-        i += period_time;
-    }
-    panic!();
-}
-
-fn check_all_rems(ts: usize, rem_freqs: &[(usize, usize)]) -> bool {
-    let mut ok = true;
-    for (rem, freq) in rem_freqs {
-        ok &= check_rems(ts, *rem, *freq);
-    }
-    ok
-}
-
-fn find_period_time(start: usize, period_time: usize, rem: usize, freq: usize) -> (usize, usize) {
-    println!(
-        "Checking: {}, {}. Start: {}, Period: {}",
-        freq, rem, start, period_time
-    );
-    let mut i = start;
-    let mut new_start = None;
-    while i < 749468541208439 {
-        if check_rems(i, rem, freq) {
-            match new_start {
-                Some(new_start) => {
-                    let new_period_time = i - new_start;
-                    return (new_start, new_period_time);
-                }
-                None => new_start = Some(i),
-            }
-        };
-        i += period_time;
-    }
-    panic!();
 }
 
 fn star_1(ts: usize, freqs: &[usize]) -> usize {
@@ -107,20 +33,44 @@ fn star_1(ts: usize, freqs: &[usize]) -> usize {
     min_f * diff
 }
 
-fn lcm(num_1: usize, num_2: usize) -> usize {
-    let (mut num, mut den) = if num_1 > num_2 {
-        (num_1, num_2)
-    } else {
-        (num_2, num_1)
-    };
-    let mut rem = num % den;
-    while rem != 0 {
-        num = den;
-        den = rem;
-        rem = num % den;
+fn check_rems(ts: usize, rem: usize, freq: usize) -> bool {
+    (ts + rem) % freq == 0
+}
+
+fn star_2(raw: impl Iterator<Item = Result<usize, std::num::ParseIntError>>) -> usize {
+    let mut rem_freqs: Vec<(usize, usize)> = raw
+        .enumerate()
+        .filter(|(_idx, f)| f.is_ok())
+        .map(|(idx, f)| (idx, f.unwrap()))
+        .collect();
+    rem_freqs.sort_unstable_by_key(|(_rem, f)| std::cmp::Reverse(*f));
+
+    let (rem, freq) = rem_freqs[0];
+    let (mut start, mut period_time) = find_period_time(1, 1, rem, freq);
+    for (rem, freq) in &rem_freqs[1..] {
+        let res = find_period_time(start, period_time, *rem, *freq);
+
+        start = res.0;
+        period_time = res.1;
     }
-    let gcd = den;
-    (num_1 * num_2) / gcd
+    start
+}
+
+fn find_period_time(start: usize, period_time: usize, rem: usize, freq: usize) -> (usize, usize) {
+    let mut i = start;
+    let mut new_start = None;
+    loop {
+        if check_rems(i, rem, freq) {
+            match new_start {
+                Some(new_start) => {
+                    let new_period_time = i - new_start;
+                    return (new_start, new_period_time);
+                }
+                None => new_start = Some(i),
+            }
+        };
+        i += period_time;
+    }
 }
 
 #[cfg(test)]
