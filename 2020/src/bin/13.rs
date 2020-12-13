3b78bc6f8c395e91ef4ecd38b11e9f2c88b9a60e
Jakob <jakob.lindqvist@chalmers.se>
2020-12-13 17:14:04 +0100

    Day 13

--- 2020/src/bin/13.rs
+++ 2020/src/bin/13.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 //749468541208439
 //219279645705120
 fn main() {
-    let file = "input/13/test";
+    let file = "input/13/input";
     let mut file = File::open(file).expect("Opening file error");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
@@ -20,37 +20,59 @@ fn main() {
         .filter(Result::is_ok)
         .map(|x| x.unwrap())
         .collect();
-    // println!("{:?}", freqs);
-    //check_rems()
     //println!("Star 1: {}", star_1(ts, &freqs));
-    //println!("Star 2: {}", star_2(raw));
+    println!("Star 2: {}", star_2(raw));
 }
+
+fn check_rems(ts: usize, rem: usize, freq: usize) -> bool {
+    (ts + rem) % freq == 0
+}
+
 fn star_2(raw: impl Iterator<Item = Result<usize, std::num::ParseIntError>>) -> usize {
     let mut rem_freqs: Vec<(usize, usize)> = raw
         .enumerate()
         .filter(|(_idx, f)| f.is_ok())
         .map(|(idx, f)| (idx, f.unwrap()))
-        .map(|(idx, f)| (idx % f, f))
         .collect();
-    //for (rem, f) in &rem_freqs {
-    //    println!("{}, {:?}", rem, f);
-    //}
-    rem_freqs.sort_unstable_by_key(|(rem, f)| std::cmp::Reverse(*f));
-    //rem_freqs.sort_unstable_by_key(|(rem, f)| std::cmp::Reverse(*f));
+    for (rem, f) in &rem_freqs {
+        println!("{}, {:?}", f, rem);
+    }
+    //let (start, period_time) = find_period_time(1, 1, 61, 17);
+
+    rem_freqs.sort_unstable_by_key(|(_rem, f)| std::cmp::Reverse(*f));
+    //rem_freqs.sort_unstable_by_key(|(rem, f)| *f);
 
     let (rem, freq) = rem_freqs[0];
-    let (mut start, mut period_time) = find_period_time(freq + rem, 1, rem, freq);
-    for (rem, freq) in &rem_freqs[1..] {
+    let (mut start, mut period_time) = find_period_time(1, 1, rem, freq);
+    println!("start: {}, period_time: {}", start, period_time);
+    for (rem, freq) in &rem_freqs[1..3] {
         let res = find_period_time(start, period_time, *rem, *freq);
 
         start = res.0;
         period_time = res.1;
+        println!("start: {}, period_time: {}", start, period_time);
+        println!("*****");
     }
-    println!("start: {}, period_time: {}", start, period_time);
-    //println!("{}", check_rems(118, &rem_freqs));
-    //let (_, start_freq) = rem_freqs.iter().max_by_key(|(_idx, f)| *f).unwrap();
-    //(mgn..).find(|ts| check_rems(*ts, &rem_freqs)).unwrap()
-    start
+    star_2_semi(start, period_time, &rem_freqs[3..])
+}
+
+fn star_2_semi(start: usize, period_time: usize, rem_freqs: &[(usize, usize)]) -> usize {
+    let mut i = start;
+    while i < 749468541208439 {
+        if check_all_rems(i, rem_freqs) {
+            return i;
+        };
+        i += period_time;
+    }
+    panic!();
+}
+
+fn check_all_rems(ts: usize, rem_freqs: &[(usize, usize)]) -> bool {
+    let mut ok = true;
+    for (rem, freq) in rem_freqs {
+        ok &= check_rems(ts, *rem, *freq);
+    }
+    ok
 }
 
 fn find_period_time(start: usize, period_time: usize, rem: usize, freq: usize) -> (usize, usize) {
@@ -64,38 +86,25 @@ fn find_period_time(start: usize, period_time: usize, rem: usize, freq: usize) -
         if check_rems(i, rem, freq) {
             match new_start {
                 Some(new_start) => {
-                    let diff = i - new_start;
-                    return (new_start, diff);
+                    let new_period_time = i - new_start;
+                    return (new_start, new_period_time);
                 }
                 None => new_start = Some(i),
             }
-        }
+        };
         i += period_time;
     }
     panic!();
 }
 
-fn check_rems(ts: usize, rem: usize, freq: usize) -> bool {
-    //println!("{}, {}", freq, rem);
-    let div = ts / freq;
-    //println!("{}, {}, {}, {}", check, rem, freq, div);
-    let div = if div == 0 { div } else { div + 1 };
-    //println!("{} == {}", (freq * div - ts) % freq, rem);
-    ((freq * div - ts) % freq) == rem
-}
-
-fn lin(f: usize, step: usize, rem: usize) -> usize {
-    f * step - rem
-}
-
-fn lcm_set(list: &[usize]) -> usize {
-    let first = list[0];
-    let second = list[1];
-    let mut lcm_ = lcm(first, second);
-    for num in &list[2..] {
-        lcm_ = lcm(lcm_, *num);
-    }
-    lcm_
+fn star_1(ts: usize, freqs: &[usize]) -> usize {
+    let (min_f, diff) = freqs
+        .iter()
+        .map(|f| (f, ts / f))
+        .map(|(f, dts)| (f, f * (dts + 1) - ts))
+        .min_by_key(|(_, diff)| *diff)
+        .unwrap();
+    min_f * diff
 }
 
 fn lcm(num_1: usize, num_2: usize) -> usize {
@@ -112,16 +121,6 @@ fn lcm(num_1: usize, num_2: usize) -> usize {
     }
     let gcd = den;
     (num_1 * num_2) / gcd
-}
-
-fn star_1(ts: usize, freqs: &[usize]) -> usize {
-    let (min_f, diff) = freqs
-        .iter()
-        .map(|f| (f, ts / f))
-        .map(|(f, dts)| (f, f * (dts + 1) - ts))
-        .min_by_key(|(_, diff)| *diff)
-        .unwrap();
-    min_f * diff
 }
 
 #[cfg(test)]
